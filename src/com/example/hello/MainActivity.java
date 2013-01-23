996c7675836d44c849d9f6971bb2eba11c2a946f
xiaojunhan <hanxiaojun85@gmail.com>
2013-01-24 00:47:48 +0800

    test

--- src/com/example/hello/MainActivity.java
+++ src/com/example/hello/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.hello;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.hello.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,47 @@ public class MainActivity extends Activity {
 	}
 
 	public void sendMessage(View view) {
-	    System.out.println("test================");
-	    AlertDialog dialog = new AlertDialog.Builder(view.getContext())
-	     .create();
-	    dialog.setMessage("hello world ,晓军中文测试 ");
-	    dialog.show();
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// AlertDialog dialog = new AlertDialog.Builder(this) .create();
+		// dialog.setMessage("hello world ,晓军中文测试 ");
+		// dialog.show();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+
+			this.showTips();
+			return false;
+		}
+
+		return false;
+	}
+
+	private void showTips() {
+
+		AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+		.setTitle("退出程序")
+		.setMessage("      是否退出程序?")
+		.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+				onDestroy();
+			}
+		}).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				return;
+			}
+		}).create(); // 创建对话框
+		alertDialog.show(); // 显示对话框
 	}
 }
